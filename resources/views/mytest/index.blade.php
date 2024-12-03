6ea6daabd8f1b361ac49a5c4fb33ac8844f61066
difarifaldi <difarifaldi0906@gmail.com>
2024-12-03 16:37:34 +0700

    myClassMyTest

--- resources/views/mytest/index.blade.php
+++ resources/views/mytest/index.blade.php
@@ -1,16 +1,8 @@
 @extends('layouts.section')
 @section('content')
-    <div class="px-3 py-1">
+    <div class="px-3 py-4">
         <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">My Test</h1>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
+
         <!-- /.content-header -->
         <!-- Main content -->
         <section class="content">
@@ -19,6 +11,9 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
+                            <div class="card-header">
+                                <h3 class="font-weight-bold">My Test</h3>
+                            </div>
                             <div class="card-body">
                                 <div class="table-responsive mt-3">
                                     <input type="hidden" id="url_dt" value="{{ $datatable_route }}">
