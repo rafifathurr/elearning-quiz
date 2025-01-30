d630e6b2b76392e4e499d1208e65881bea9d623c
difarifaldi <difarifaldi0906@gmail.com>
2025-01-30 14:35:51 +0700

    perbaiki tampilan kecermatan di hp

--- resources/views/master/kecermatan/play/question.blade.php
+++ resources/views/master/kecermatan/play/question.blade.php
@@ -1,6 +1,124 @@
+<style>
+    .card-container,
+    .question-container {
+        display: flex;
+        justify-content: center;
+        flex-wrap: nowrap;
+        gap: 15px;
+        overflow-x: auto;
+        padding: 5px;
+    }
+
+    .card-container .card {
+        flex: 1 1 auto;
+        max-width: 18%;
+        display: flex;
+        padding: 15px;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        position: relative;
+    }
+
+    .question-container .card {
+        flex: 1 1 auto;
+        max-width: 18%;
+        display: flex;
+        padding: 15px;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        position: relative;
+        margin-top: 40px;
+    }
+
+    /* Label di luar kartu */
+    .card-label {
+        position: absolute;
+        top: -30px;
+        /* Menempatkan label di luar kartu dengan sedikit jarak */
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: 18px;
+        font-weight: bold;
+        z-index: 1;
+        background-color: white;
+        padding: 0 5px;
+    }
+
+    /* Card body styling */
+    .card-container .card-body,
+    .question-container .card-body {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        padding: 0;
+    }
+
+    /* Responsif */
+    @media (min-width: 540px) {
+
+        .card-container,
+        .question-container {
+            gap: 20px;
+        }
+
+        .card-container .card,
+        .question-container .card {
+            max-width: 25%;
+            padding: 20px;
+            margin-top: 40px;
+            /* Memberikan ruang untuk label */
+        }
+
+        .card-label {
+            position: absolute;
+            top: -40px;
+            /* Menyesuaikan posisi label untuk ukuran lebih besar */
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 25px;
+            font-weight: bold;
+            z-index: 1;
+            background-color: white;
+            padding: 0 5px;
+        }
+    }
+
+    @media (min-width: 1024px) {
+
+        .card-container,
+        .question-container {
+            gap: 30px;
+        }
+
+        .card-container .card,
+        .question-container .card {
+            max-width: 30%;
+            padding: 25px;
+            margin-top: 50px;
+            /* Memberikan lebih banyak ruang untuk label */
+        }
+
+        .card-label {
+            position: absolute;
+            top: -45px;
+            /* Menyesuaikan posisi label untuk ukuran lebih besar */
+            left: 50%;
+            transform: translateX(-50%);
+            font-size: 30px;
+            font-weight: bold;
+            z-index: 1;
+            background-color: white;
+            padding: 0 5px;
+        }
+    }
+</style>
 <div class="row">
 
-    <div class="col-lg-12 col-md-12">
+    <div class="col-12">
         <div class="card">
             <div class="p-2">
                 <h2>
@@ -19,52 +137,51 @@
                     <div class="card">
                         <div class="card-body">
 
-                            <div class="d-flex flex-wrap justify-content-center">
-                                <h3 class="text-center"></h3>
-                                @foreach ($unique_answers as $index => $answer)
-                                    <div class="p-2 text-center">
-                                        <h3 class="font-weight-bold">
-                                            {{ chr(65 + $index) }} <!-- Convert index to A, B, C, etc. -->
-                                        </h3>
-                                        <div class="card m-2 px-2 bg-white text-dark border">
+                            <div class="d-flex flex-wrap justify-content-center ">
+                                <div class="question-container">
+                                    @foreach ($unique_answers as $index => $answer)
+                                        <div class="card bg-white text-dark border">
+                                            <h3 class="font-weight-bold card-label bg-white">
+                                                {{ chr(65 + $index) }} <!-- Convert index to A, B, C, etc. -->
+                                            </h3>
                                             <div class="card-body">
                                                 <h4 class="font-weight-bold text-center my-auto">
                                                     {{ $answer }}
                                                 </h4>
                                             </div>
                                         </div>
-                                    </div>
-                                @endforeach
+                                    @endforeach
+                                </div>
                             </div>
 
                             <h3 class="font-weight-bold text-center mt-3">
                                 SOAL
                             </h3>
                             <div class="d-flex flex-wrap justify-content-center">
-                                @foreach ($active_question['quiz_answer'] as $quiz_answer)
-                                    <div class="p-2 text-center">
-                                        <div class="card m-2 px-2 bg-white text-dark border">
+                                <div class="card-container">
+                                    @foreach ($active_question['quiz_answer'] as $quiz_answer)
+                                        <div class="card text-center bg-white text-dark border">
                                             <div class="card-body">
                                                 <h4 class="font-weight-bold text-center my-auto">
                                                     {{ $quiz_answer['answer'] }}
                                                 </h4>
                                             </div>
                                         </div>
-                                    </div>
-                                @endforeach
+                                    @endforeach
+                                </div>
                             </div>
 
 
 
 
                             <div class="border-top " id="answer_list">
-                                <div class="col-lg-12 py-3 mt-4 d-flex flex-wrap justify-content-center col-md-4">
+                                <div class="py-3 mt-4 d-flex flex-wrap justify-content-center">
                                     @php
                                         $index_new = 0;
                                     @endphp
-                                    @foreach ($unique_answers as $index => $answer)
-                                        <div class="p-2 text-center">
-                                            <div class="card my-2 mx-4 px-2 bg-white text-dark border"
+                                    <div class="card-container">
+                                        @foreach ($unique_answers as $index => $answer)
+                                            <div class="card  text-center bg-white text-dark border"
                                                 style="cursor: pointer;" onclick="answerKecermatan(this)">
                                                 <input type="hidden" name="answer_list" value="{{ $answer }}">
                                                 <div class="card-body">
@@ -73,10 +190,11 @@
                                                     </h4>
                                                 </div>
                                             </div>
-                                        </div>
-                                    @endforeach
+                                        @endforeach
+                                    </div>
                                 </div>
                             </div>
+
                             <input type="hidden" id="url-next"
                                 value="{{ route('kecermatan.getQuestion', ['result' => $result->id]) . '?q=' . $active_question['question_number'] + 1 }}">
 
