25a5ba5109bca69700e8501e3681d70c628cd395
difarifaldi <difarifaldi0906@gmail.com>
2025-02-04 16:47:23 +0700

    perbaiki tampilan card di landing page

--- resources/views/master/package_payment/package_list.blade.php
+++ resources/views/master/package_payment/package_list.blade.php
@@ -1,13 +1,23 @@
 <ul class="text-center list-unstyled p-2 m-0">
     @forelse ($packages as $package)
         <li class="mb-3">
-            <button onclick="checkOut({{ $package->id }}, '{{ $package->name }}')"
-                class="btn btn-primary w-100 rounded-lg shadow-sm" style="font-size: 1.1rem;">
-                {{ $package->name }} <br>
-                <span class="bg-white p-1 font-weight-bold rounded" style="font-size: 0.9rem;">
-                    {{ $package->price > 0 ? 'Rp. ' . number_format($package->price, 0, ',', '.') : 'Gratis' }}
-                </span>
-            </button>
+            @if (Auth::check())
+                <button onclick="checkOut({{ $package->id }}, '{{ $package->name }}')"
+                    class="btn btn-primary w-100 rounded-lg shadow-sm" style="font-size: 1.1rem;">
+                    {{ $package->name }} <br>
+                    <span class="bg-white p-1 font-weight-bold rounded" style="font-size: 0.9rem;">
+                        {{ $package->price > 0 ? 'Rp. ' . number_format($package->price, 0, ',', '.') : 'Gratis' }}
+                    </span>
+                </button>
+            @else
+                <a href="{{ route('login') }}" class="btn btn-primary w-full d-block font-weight-bold p-2 rounded-lg"
+                    style="font-size: 1.2rem;">{{ $package->name }}
+                    <br>
+                    <span class="bg-white p-1 font-weight-bold rounded" style="font-size: 0.9rem;">
+                        {{ $package->price > 0 ? 'Rp. ' . number_format($package->price, 0, ',', '.') : 'Gratis' }}
+                    </span>
+                </a>
+            @endif
         </li>
     @empty
         <p class="font-weight-bolder text-danger">-- Belum Ada Paket --</p>
