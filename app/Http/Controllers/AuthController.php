39f03fc5098ed4d0fa4a447efeef37a980af8e6e
difarifaldi <difarifaldi0906@gmail.com>
2025-02-04 10:15:43 +0700

    google login kondisi masih tes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -67,7 +67,36 @@ class AuthController extends Controller
         $user->assignRole('user');
 
         Auth::login($user);
-        return redirect('/home')->with('success', 'Registrasi berhasil!');
+        $result = Result::where('user_id', $user->id)
+            ->whereNull('finish_time')
+            ->first();
+
+        $currentDateTime = \Carbon\Carbon::now();
+        if ($result) {
+            $startTime = \Carbon\Carbon::parse($result->start_time);
+            $endTime = $startTime->copy()->addSeconds($result->time_duration);
+
+            // Cek jika waktu belum habis dan quiz belum selesai
+            if ($currentDateTime->lte($endTime)) {
+                $remainingSeconds = $endTime->timestamp - $currentDateTime->timestamp;
+                if ($result->quiz->type_aspect == 'kecermatan') {
+                    return redirect()->route('kecermatan.getQuestion', ['result' => $result->id, 'remaining_time' => encrypt($remainingSeconds)]);
+                } else {
+                    return redirect()->route('admin.quiz.getQuestion', ['result' => $result->id, 'remaining_time' => encrypt($remainingSeconds)]);
+                }
+            } else {
+                // Jika sudah selesai, update dan redirect ke halaman yang tepat
+                $total_score = ResultDetail::where('result_id', $result->id)->sum('score');
+                $result->update([
+                    'finish_time' => $endTime,
+                    'total_score' => $total_score
+                ]);
+                return redirect()->route('home')->with(['success' => 'Login Berhasil']);
+            }
+        } else {
+            // Jika result tidak ditemukan, redirect ke halaman lain
+            return redirect()->route('home')->with(['success' => 'Login Berhasil']);
+        }
     }
 
     public function login()
