d24f7f978fd2894192b9ad341e3fbb90db885821
difarifaldi <difarifaldi0906@gmail.com>
2025-02-06 13:53:02 +0700

    perbaiki button back edit user

--- resources/views/master/user/edit.blade.php
+++ resources/views/master/user/edit.blade.php
@@ -116,7 +116,7 @@
                                 </div>
 
                                 <div class="pt-3 d-flex">
-                                    <a href="{{ route('master.user.index') }}" class="btn btn-danger mr-2"> Back</a>
+                                    <a href="{{ url()->previous() }}" class="btn btn-danger mr-2">Back</a>
                                     <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
                             </div>
