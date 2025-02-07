5395c4d1a3f9fc464b58657314c733195ada1c99
difarifaldi <difarifaldi0906@gmail.com>
2025-02-07 11:16:57 +0700

    tambah jumlah pertemuan paket

--- resources/views/master/package_payment/package_list.blade.php
+++ resources/views/master/package_payment/package_list.blade.php
@@ -3,8 +3,15 @@
         <li class="mb-3">
             @if (Auth::check())
                 <button onclick="checkOut({{ $package->id }}, '{{ $package->name }}')"
-                    class="btn btn-primary w-100 rounded-lg shadow-sm" style="font-size: 1.1rem;">
+                    class="btn btn-primary w-100 rounded-lg shadow-sm position-relative" style="font-size: 1.1rem;">
                     {{ $package->name }} <br>
+
+                    @if (isset($package->class) && $package->class > 0)
+                        <span class="font-weight-bold " style="font-size: 1rem">
+
+                            {{ $package->class }}x Pertemuan
+                        </span><br>
+                    @endif
                     <span class="bg-white p-1 font-weight-bold rounded" style="font-size: 0.9rem;">
                         {{ $package->price > 0 ? 'Rp. ' . number_format($package->price, 0, ',', '.') : 'Gratis' }}
                     </span>
@@ -13,6 +20,12 @@
                 <a href="{{ route('login') }}" class="btn btn-primary w-full d-block font-weight-bold p-2 rounded-lg"
                     style="font-size: 1.2rem;">{{ $package->name }}
                     <br>
+                    @if (isset($package->class) && $package->class > 0)
+                        <span class="font-weight-bold " style="font-size: 1rem">
+
+                            {{ $package->class }}x Pertemuan
+                        </span><br>
+                    @endif
                     <span class="bg-white p-1 font-weight-bold rounded" style="font-size: 0.9rem;">
                         {{ $package->price > 0 ? 'Rp. ' . number_format($package->price, 0, ',', '.') : 'Gratis' }}
                     </span>
