5992d54e57af1f197b7c88fe881a702c185efc9d
difarifaldi <difarifaldi0906@gmail.com>
2025-02-14 14:28:17 +0700

    hide container upload payment ketika sudah upload

--- resources/views/order/detail-transfer.blade.php
+++ resources/views/order/detail-transfer.blade.php
@@ -79,51 +79,54 @@
                                     class="fas fa-arrow-left mr-1"></i>Kembali</a>
                         </div>
                     </div>
+                    @if ($order->status == 2)
+                        <div class="callout callout-info">
+                            <h5><i class="fas fa-university"></i> Informasi Pembayaran</h5>
+                            <p>Silakan lakukan pembayaran ke rekening berikut:</p>
 
-                    <div class="callout callout-info">
-                        <h5><i class="fas fa-university"></i> Informasi Pembayaran</h5>
-                        <p>Silakan lakukan pembayaran ke rekening berikut:</p>
-
-                        <div class="d-flex flex-wrap align-items-center p-2 bg-white rounded shadow-sm">
-                            <img src="{{ asset('img/brilogo.png') }}" alt="BRI Logo" class="img-fluid mr-2"
-                                style="height: 30px;">
-                            <span class="fw-bold badge text-white px-3 py-2 mr-2" style="background-color: #0A3D91;">Bank
-                                BRI</span>
-                            <div class="d-flex flex-column">
-                                <strong id="rekening" class="text-dark">038501001542300</strong>
-                                <small class="text-muted">a.n. ATLAS KAPITAL PERKASA</small>
-                            </div>
-                            <button class="btn btn-sm btn-outline-primary ml-auto" onclick="copyRekening()">
-                                <i class="fas fa-copy"></i> Copy
-                            </button>
-                        </div>
-                        <small class="text-success" id="copy-alert" style="display: none;">Nomor rekening berhasil
-                            disalin!</small>
-
-                        <div class="card mt-4 border shadow-sm">
-                            <div class="card-body">
-                                <div class="alert alert-default-info" role="alert">
-                                    <h6 class="alert-heading"> Berita:
-                                        <strong>"{{ 'BC' . $year . $order->id . '-' . $order->user->name }}"</strong>
-                                    </h6>
+                            <div class="d-flex flex-wrap align-items-center p-2 bg-white rounded shadow-sm">
+                                <img src="{{ asset('img/brilogo.png') }}" alt="BRI Logo" class="img-fluid mr-2"
+                                    style="height: 30px;">
+                                <span class="fw-bold badge text-white px-3 py-2 mr-2"
+                                    style="background-color: #0A3D91;">Bank
+                                    BRI</span>
+                                <div class="d-flex flex-column">
+                                    <strong id="rekening" class="text-dark">038501001542300</strong>
+                                    <small class="text-muted">a.n. ATLAS KAPITAL PERKASA</small>
                                 </div>
-                                <form action="{{ route('order.uploadPayment', ['id' => $order->id]) }}" method="post"
-                                    enctype="multipart/form-data">
-                                    @csrf
-                                    @method('patch')
-                                    <div class="form-group row ">
-                                        <label for="proof_payment" class="col-md-4 control-label text-left">Upload Bukti
-                                            Pembayaran <span class="text-danger">*</span></label>
-                                        <input type="file" class="form-control" name="proof_payment" id="proof_payment"
-                                            accept="image/jpeg,image/jpg,image/png" required>
+                                <button class="btn btn-sm btn-outline-primary ml-auto" onclick="copyRekening()">
+                                    <i class="fas fa-copy"></i> Copy
+                                </button>
+                            </div>
+                            <small class="text-success" id="copy-alert" style="display: none;">Nomor rekening berhasil
+                                disalin!</small>
+
+                            <div class="card mt-4 border shadow-sm">
+                                <div class="card-body">
+                                    <div class="alert alert-default-info" role="alert">
+                                        <h6 class="alert-heading"> Berita:
+                                            <strong>"{{ 'BC' . $year . $order->id . '-' . $order->user->name }}"</strong>
+                                        </h6>
                                     </div>
-                                    <button type="submit" class="btn btn-primary btn-sm">
-                                        Kirim
-                                    </button>
-                                </form>
+                                    <form action="{{ route('order.uploadPayment', ['id' => $order->id]) }}" method="post"
+                                        enctype="multipart/form-data">
+                                        @csrf
+                                        @method('patch')
+                                        <div class="form-group row ">
+                                            <label for="proof_payment" class="col-md-4 control-label text-left">Upload Bukti
+                                                Pembayaran <span class="text-danger">*</span></label>
+                                            <input type="file" class="form-control" name="proof_payment"
+                                                id="proof_payment" accept="image/jpeg,image/jpg,image/png" required>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-sm">
+                                            Kirim
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
+
                 </div>
             </div>
         </div>
