16cb18069e48c63a48d1200d1d77e93abb6d6861
difarifaldi <difarifaldi0906@gmail.com>
2025-04-10 15:56:51 +0700

    perbaiki script firebase

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -74,36 +74,6 @@
 <!-- FireBase -->
 @auth
     <script type="module">
-        // Import the functions you need from the SDKs you need
-        import {
-            initializeApp
-        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-        import {
-            getAnalytics
-        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-        import {
-            getMessaging
-        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging.js";
-
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-            apiKey: "AIzaSyAxX4jUdibL9n8rJqEeEnxqPKu3S1uAQ9k",
-            authDomain: "brata-cerdas-1.firebaseapp.com",
-            projectId: "brata-cerdas-1",
-            storageBucket: "brata-cerdas-1.firebasestorage.app",
-            messagingSenderId: "319745808387",
-            appId: "1:319745808387:web:de0c26d961571816af1636",
-            measurementId: "G-XE79X5SH5R"
-        };
-
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-        const messaging = getMessaging(app);
         getToken(messaging, {
             vapidKey: "BKCYJjmPhEQ9LKpeyxSy7Ui1FhhGcC5Rz6W6L08he9rr6ZEShmx_U8d9HcIC7qzbzM-Hwl-uQzgnY24ij18U-xs"
         }).then((currentToken) => {
