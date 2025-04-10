ef5c7ef52bbc14719c3e1acbe0e0e60a20e19d1d
difarifaldi <difarifaldi0906@gmail.com>
2025-04-10 15:50:15 +0700

    pindahin firebase

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,54 +1,3 @@
-<!-- FireBase -->
-@auth
-    <script type="module">
-        // Import the functions you need from the SDKs you need
-        import {
-            initializeApp
-        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-        import {
-            getAnalytics
-        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-        import {
-            getMessaging
-        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging.js";
-
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-            apiKey: "AIzaSyAxX4jUdibL9n8rJqEeEnxqPKu3S1uAQ9k",
-            authDomain: "brata-cerdas-1.firebaseapp.com",
-            projectId: "brata-cerdas-1",
-            storageBucket: "brata-cerdas-1.firebasestorage.app",
-            messagingSenderId: "319745808387",
-            appId: "1:319745808387:web:de0c26d961571816af1636",
-            measurementId: "G-XE79X5SH5R"
-        };
-
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-        const messaging = getMessaging(app);
-        getToken(messaging, {
-            vapidKey: "BKCYJjmPhEQ9LKpeyxSy7Ui1FhhGcC5Rz6W6L08he9rr6ZEShmx_U8d9HcIC7qzbzM-Hwl-uQzgnY24ij18U-xs"
-        }).then((currentToken) => {
-            if (currentToken) {
-                // Send the token to your server and update the UI if necessary
-                // ...
-            } else {
-                // Show permission request UI
-                console.log('No registration token available. Request permission to generate one.');
-                // ...
-            }
-        }).catch((err) => {
-            console.log('An error occurred while retrieving token. ', err);
-            // ...
-        });
-    </script>
-@endauth
-
 <!-- jQuery -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
@@ -121,3 +70,54 @@
         });
     });
 </script>
+
+<!-- FireBase -->
+@auth
+    <script type="module">
+        // Import the functions you need from the SDKs you need
+        import {
+            initializeApp
+        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+        import {
+            getAnalytics
+        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+        import {
+            getMessaging
+        } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging.js";
+
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyAxX4jUdibL9n8rJqEeEnxqPKu3S1uAQ9k",
+            authDomain: "brata-cerdas-1.firebaseapp.com",
+            projectId: "brata-cerdas-1",
+            storageBucket: "brata-cerdas-1.firebasestorage.app",
+            messagingSenderId: "319745808387",
+            appId: "1:319745808387:web:de0c26d961571816af1636",
+            measurementId: "G-XE79X5SH5R"
+        };
+
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+        const messaging = getMessaging(app);
+        getToken(messaging, {
+            vapidKey: "BKCYJjmPhEQ9LKpeyxSy7Ui1FhhGcC5Rz6W6L08he9rr6ZEShmx_U8d9HcIC7qzbzM-Hwl-uQzgnY24ij18U-xs"
+        }).then((currentToken) => {
+            if (currentToken) {
+                // Send the token to your server and update the UI if necessary
+                // ...
+            } else {
+                // Show permission request UI
+                console.log('No registration token available. Request permission to generate one.');
+                // ...
+            }
+        }).catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+            // ...
+        });
+    </script>
+@endauth
