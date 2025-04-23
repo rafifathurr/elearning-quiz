60d49c1a502452b41d0ff771793991074c33aba9
difarifaldi <difarifaldi0906@gmail.com>
2025-04-23 17:51:41 +0700

    pindahin script keatas

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -70,30 +70,3 @@
         });
     });
 </script>
-
-<!-- FireBase -->
-@auth
-    <script type="module">
-        $(document).ready(function() {
-            // Initialize Firebase
-            const app = initializeApp(firebaseConfig);
-            const analytics = getAnalytics(app);
-            const messaging = getMessaging(app);
-            getToken(messaging, {
-                vapidKey: "BKCYJjmPhEQ9LKpeyxSy7Ui1FhhGcC5Rz6W6L08he9rr6ZEShmx_U8d9HcIC7qzbzM-Hwl-uQzgnY24ij18U-xs"
-            }).then((currentToken) => {
-                if (currentToken) {
-                    // Send the token to your server and update the UI if necessary
-                    // ...
-                } else {
-                    // Show permission request UI
-                    console.log('No registration token available. Request permission to generate one.');
-                    // ...
-                }
-            }).catch((err) => {
-                console.log('An error occurred while retrieving token. ', err);
-                // ...
-            });
-        });
-    </script>
-@endauth
